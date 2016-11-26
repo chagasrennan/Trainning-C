1e423f1f2e8c27329704d9376211a1465f0e7c13
chagasrennan <chagasrennan@gmail.com>
2016-11-25 22:37:28 -0200

    Pi montecarlo closed

--- pi-calculation/montecarlo.cpp
+++ pi-calculation/montecarlo.cpp
@@ -10,41 +10,39 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <iomanip>      // std::setprecision
 
 using namespace std;
-
 
 int main()
 {
 	srand(time(NULL));
 
-	int x; // real number [0,1]
-	int y; // real number [0,1]
-
-	int n = 1000; // number of iterations
-
+	float x; // real number [0,1]
+	float y; // real number [0,1]
+	int n = 1000000; // number of iterations
 	int ctin; // counter of points inside circle
-	int lepi; // value of pi
-
-	int aux; // auxliary variable
+	float lepi; // value of pi
+	float aux; // auxliary variable
 
 	for (int i = 0; i < n; ++i)
 	{
-		x = rand();
-		y = rand();
+		x = ((float) rand() / (RAND_MAX)); // sample a [0.1] number for x
+		y = ((float) rand() / (RAND_MAX)); // sample a [0.1] number for y
 
-		aux = x^2 + y^2;
-		
-		if ( aux < 1)
+		aux = x*x + y*y;
+		if ( aux <= 1)
 		{
-			ctin = ctin++;
+			ctin = ctin+1;
 		}
 	}
+	// cout << "\nIterations: " << n << "\tInside: " << ctin << endl;
 
-	lepi = 4*(ctin/n);
-
-	cout << "Pi: " << lepi;
-
+	lepi = 4.0*(1.0*ctin / 1.0*n); // this part was tricky
+	// I had to add 1.0 multipling ctin and n 
+	// ctin and n are int type, thus, ctin/n returned a int
+	// need to find a better way to get the double/float 
+	cout << "\nPi after " << n << " itearations: " << setprecision(12) << lepi << "\n\n";
 	return 0;
 }
 
