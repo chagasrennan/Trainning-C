97b34beff8fdbc3accb9b974856afb0ea1ee0b2a
chagasrennan <chagasrennan@gmail.com>
2017-03-10 16:05:09 -0300

    double function returning fahrenheit temperature

---
+++ functions/celsiusTofahrenheit.cpp
@@ -0,0 +1,35 @@
+/*
+==================================================================
+    C++ training - Function that converts Celsius temperature to 
+    				Fahrenheit  
+
+ Author: Rennan Danilo Seimetz Chagas
+ email: chagasrennan@gmail.com
+==================================================================
+*/
+
+#include <iostream>
+#include <fstream> // class to reado and writ from/to files
+
+
+double ctof(double c_temp)
+{
+	double f_temp;
+	f_temp = (9*c_temp/5)+32;
+	return((9*c_temp/5)+32);
+}
+
+int main()
+{
+
+	using namespace std;
+	double c_temp;
+	double f_temp;
+
+	cout << "\nEnter Celsius temperature: ";
+	cin >> c_temp;
+	f_temp = ctof(c_temp);
+
+	cout << "Temperature in Fahrenheit: " << f_temp << "\n\n";
+	return 0;
+}
